ca35bd1b37261a5e373b6359678d70bda316e922
DanielPRe15 <dani123pre@gmail.com>
2023-10-22 20:48:41 -0500

    Formulario Mecanico

---
+++ app/src/main/java/com/example/aplicacionservicios/ServicioMecanicoActivity.kt
@@ -0,0 +1,41 @@
+package com.example.aplicacionservicios
+
+import android.os.Bundle
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+
+class ServicioMecanicoActivity : AppCompatActivity() {
+
+    private lateinit var txtCliente : TextInputEditText
+    private lateinit var txtTelefono : TextInputEditText
+    private lateinit var txtFecha : TextInputEditText
+    private lateinit var txtDireccion : TextInputEditText
+    private lateinit var txtInformacion : TextInputEditText
+
+    private lateinit var atvServicio : AutoCompleteTextView
+
+    private lateinit var btnSiguiente : Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mecanico_main)
+
+        txtCliente = findViewById(R.id.txtCliente)
+         txtTelefono = findViewById(R.id.txtTelefono)
+        txtFecha = findViewById(R.id.txtFecha)
+        txtDireccion = findViewById(R.id.txtDireccion)
+        txtInformacion = findViewById(R.id.txtInformacion)
+        atvServicio = findViewById(R.id.atvServicio)
+        btnSiguiente = findViewById(R.id.btnSiguiente)
+        btnSiguiente.setOnClickListener{siguiente()}
+    }
+
+    fun siguiente()
+    {
+
+    }
+
+}
